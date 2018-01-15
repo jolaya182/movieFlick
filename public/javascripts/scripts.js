c9b9399e0ae49135001dbfef0a6bbe7c27e356bf
jolaya182 <jolaya182@gmail.com>
2018-01-15 14:45:24 -0500

    formated

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,9 +2,9 @@ var previousResults = [];
 
 
 // Defined our selectors and grabbed key elements from the page
-var form     = document.querySelector("#searchForm");
-var result   = document.querySelector("#result");
-var sort     = document.querySelector("#sort");
+var form = document.querySelector("#searchForm");
+var result = document.querySelector("#result");
+var sort = document.querySelector("#sort");
 var previous = document.querySelector("#previousResults");
 
 form.addEventListener("submit", function (event) {
@@ -33,29 +33,29 @@ sort.addEventListener("change", function () {
   renderPrevious(sorted);
 });
 
-function sortResults(results, strategy){
-    switch(strategy){
+function sortResults(results, strategy) {
+  switch (strategy) {
     case 'default':
       return results;
     case 'title':
       return results.sort(sortByTitle);
     case 'rating':
       return results.sort(sortByRating);
-    }
+  }
 }
 
-function sortByTitle(a, b){
+function sortByTitle(a, b) {
   //this will ensure a sorted array by names
-  if(a.results[0].original_title < b.results[0].original_title){return -1}
+  if (a.results[0].original_title < b.results[0].original_title) { return -1 }
 
-    if(a.results[0].original_title > b.results[0].original_title){return 1}
+  if (a.results[0].original_title > b.results[0].original_title) { return 1 }
 
   return 0;
 }
 
-function sortByRating(a, b){
+function sortByRating(a, b) {
 
-  return Math.floor((a.results[0].vote_average - b.results[0].vote_average)*10);
+  return Math.floor((a.results[0].vote_average - b.results[0].vote_average) * 10);
 }
 
 // Create a search for title
@@ -63,7 +63,7 @@ function sortByRating(a, b){
 function createSearch(title) {
   // does url encoding for us, i.e. for the title
 
-  return $.get('http://api.themoviedb.org/3/search/movie?&api_key=8aa5496a879aacdb95070bbd5121ff40&query='+  title );
+  return $.get('http://api.themoviedb.org/3/search/movie?&api_key=8aa5496a879aacdb95070bbd5121ff40&query=' + title);
 }
 
 // Updating Current Result
@@ -99,26 +99,26 @@ function renderPrevious(prev) {
 function buildResult(result) {
 
   return '<column class="result">' +
-              '<img class=resultingImg src=http://image.tmdb.org/t/p/w500'+result.results[0].poster_path+' />' +
-              buildTitle(result.results[0].original_title) +
-              buildPlot(result.results[0].overview) +
-              buildRating(result.results[0].vote_average) +
-           '</column>';
+    '<img class=resultingImg src=http://image.tmdb.org/t/p/w500' + result.results[0].poster_path + ' />' +
+    buildTitle(result.results[0].original_title) +
+    buildPlot(result.results[0].overview) +
+    buildRating(result.results[0].vote_average) +
+    '</column>';
 }
 
 
 
 // How could we clean up these build functions?
-  // Don't they all kind of look the same?
-  // Could you use that to improve your buildResult?
+// Don't they all kind of look the same?
+// Could you use that to improve your buildResult?
 function buildTitle(title) {
-  return ('<h2 class="title">' + title  + '</h2>');
+  return ('<h2 class="title">' + title + '</h2>');
 }
 
 function buildPlot(plot) {
-  return ('<p class="description">' + plot  + '</p>');
+  return ('<p class="description">' + plot + '</p>');
 }
 
 function buildRating(rating) {
-  return ('<p class="rating">' +  rating  + '</p>');
+  return ('<p class="rating">' + rating + '</p>');
 }
